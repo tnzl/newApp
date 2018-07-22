af699682e431f80c4b2735183e0e6a93d52a546e
Tanzeel <iamtanzeel1998@gmail.com>
2018-07-23 02:15:51 +0530

    added more info Activity.

---
+++ app/src/main/java/com/example/tanzeel/newapp/moreInfo.java
@@ -0,0 +1,13 @@
+package com.example.tanzeel.newapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class moreInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_more_info);
+    }
+}
